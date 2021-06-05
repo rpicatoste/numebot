3998c9b47a3a2adb21af29923e01eada1818ee1f
Ricardo Picatoste <rpicatoste@gmail.com>
2021-06-05 15:44:13 +0200

    Improve final correlations plot.

--- numebot/monitoring/dashboard/dashboard_main.py
+++ numebot/monitoring/dashboard/dashboard_main.py
@@ -2,11 +2,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.graph_objects as go
 
 from numebot.data.data_constants import NC
 from numebot.env import NUMERAI_DATA_FOLDER, MODEL_CONFIGS_PATH
-from numebot.monitoring.dashboard.dashboard_functions import get_color_dict, get_shape_dict
+from numebot.monitoring.dashboard.dashboard_plots import plot_daily_correlations, plot_final_correlations
 from numebot.round_manager import RoundManager
 from numebot.secret import PUBLIC_ID, SECRET_KEY
 
@@ -85,32 +84,9 @@ app.layout = html.Div(
               Input("checklist_rounds", "value"),
               Input("checklist_models", "value"))
 def update_line_chart(rounds, model_names):
+
+    fig = plot_daily_correlations(full_df, rounds, model_names)
     
-    # I get the dicts including all rounds and models so colors and shapes will be consistent after 
-    # filtering.
-    shape_dict = get_shape_dict(list_of_items=full_df[NC.round].unique())
-    colors_dict = get_color_dict(list_of_items=full_df[NC.model_name].unique())
-
-    fig = go.Figure()
-    for model_name, model_df in full_df.groupby(NC.username):
-        if model_name not in model_names:
-            continue
-
-        for round_number, round_df in model_df.groupby(NC.round):
-            if round_number not in rounds:
-                continue
-            
-            fig.add_trace(go.Scatter(x=round_df[NC.date], 
-                                     y=round_df[NC.correlation],
-                                     line=dict(color=colors_dict[model_name],
-                                               dash=shape_dict[round_number]),
-                                     name=f'{model_name}-r{round_number}'))
-    
-    fig.add_hline(y=0.0)
-    fig.update_layout(title='Correlation of each model and submission',
-                      xaxis_title='Date',
-                      yaxis_title='Correlation')
-
     return fig
 
 
@@ -119,29 +95,10 @@ def update_line_chart(rounds, model_names):
               Input("checklist_models", "value"))
 def update_line_chart(rounds, model_names):
     
-    # Filter DataFrame keeping the selected models and rounds to plot, and for this plot only
-    # the last value for each model and round.
-    final_values_df = full_df.sort_values(NC.date).copy()
-    final_values_df = final_values_df.drop_duplicates([NC.model_name, NC.round], keep='last') 
-    final_values_df = final_values_df[final_values_df[NC.round].isin(rounds)]
-
-    colors_dict = get_color_dict(list_of_items=final_values_df[NC.model_name].unique())
-    
-    fig = go.Figure()
-    for model_name, model_df in final_values_df.groupby(NC.username):
-        if model_name not in model_names:
-            continue
-
-        fig.add_trace(go.Scatter(x=model_df[NC.date], 
-                                 y=model_df[NC.correlation],
-                                 line=dict(color=colors_dict[model_name]),
-                                 name=f'{model_name}'))
-    
-    fig.add_hline(y=0.0)
-    fig.update_layout(title='Final correlation of each model for each round',
-                      xaxis_title='Date',
-                      yaxis_title='Correlation')
+    fig = plot_final_correlations(full_df, rounds, model_names)
 
     return fig
 
-app.run_server(debug=True, host= '0.0.0.0')
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host= '0.0.0.0')
---
+++ numebot/monitoring/dashboard/dashboard_plots.py
@@ -0,0 +1,63 @@
+import plotly.graph_objects as go
+
+from numebot.data.data_constants import NC
+from numebot.monitoring.dashboard.dashboard_functions import get_color_dict, get_shape_dict
+
+
+def plot_daily_correlations(full_df, rounds, model_names):
+
+    # I get the dicts including all rounds and models so colors and shapes will be consistent after 
+    # filtering.
+    shape_dict = get_shape_dict(list_of_items=full_df[NC.round].unique())
+    colors_dict = get_color_dict(list_of_items=full_df[NC.model_name].unique())
+
+    fig = go.Figure()
+    for model_name, model_df in full_df.groupby(NC.username):
+        if model_name not in model_names:
+            continue
+
+        for round_number, round_df in model_df.groupby(NC.round):
+            if round_number not in rounds:
+                continue
+            
+            fig.add_trace(go.Scatter(x=round_df[NC.date], 
+                                     y=round_df[NC.correlation],
+                                     line=dict(color=colors_dict[model_name],
+                                               dash=shape_dict[round_number]),
+                                     name=f'{model_name}-r{round_number}'))
+    
+    fig.add_hline(y=0.0)
+    fig.update_layout(title='Correlation of each model and submission',
+                      xaxis_title='Date',
+                      yaxis_title='Correlation')
+
+    return fig
+
+
+def plot_final_correlations(full_df, rounds, model_names):
+    # Filter DataFrame keeping the selected models and rounds to plot, and for this plot only
+    # the last value for each model and round.
+    colors_dict = get_color_dict(list_of_items=full_df[NC.model_name].unique())
+    
+    final_values_df = full_df.sort_values(NC.date).copy().reset_index(drop=True)
+    final_values_df = final_values_df.drop_duplicates([NC.model_name, NC.round], keep='last') 
+    final_values_df = final_values_df[final_values_df[NC.round].isin(rounds)]
+
+    
+    fig = go.Figure()
+    for model_name, model_df in final_values_df.groupby(NC.username):
+        if model_name not in model_names:
+            continue
+        
+        model_df.sort_values(NC.round, inplace=True)
+        fig.add_trace(go.Scatter(x=model_df[NC.round], 
+                                 y=model_df[NC.correlation],
+                                 line=dict(color=colors_dict[model_name]),
+                                 name=f'{model_name}'))
+    
+    fig.add_hline(y=0.0)
+    fig.update_layout(title='Final correlation of each model for each round',
+                      xaxis_title='Round',
+                      yaxis_title='Correlation')
+
+    return fig
