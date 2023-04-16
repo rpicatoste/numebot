e72fbb7c7c82c666a55adb808684c5b7f5152fc2
Ricardo Picatoste <rpicatoste@gmail.com>
2023-04-16 10:27:36 +0200

    Update bash script to use the poetry env.

--- api/run_numerai_weekly.sh
+++ api/run_numerai_weekly.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-source /home/pica/anaconda3/bin/activate automl
+
+#source /home/pica/anaconda3/bin/activate automl
+echo "Running with poetry"
 cd /home/pica/workspace/numebot || exit
 
 LOG_PATH=/home/pica/nas_pica/Data/numerai/weekly_log/log_numerai_"$(date +"%Y-%m-%d_%H%M%S")".txt
-python api/run_it.py |& tee "$LOG_PATH"
+poetry run python api/run_it.py |& tee "$LOG_PATH"
